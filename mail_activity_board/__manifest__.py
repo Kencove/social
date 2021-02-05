3af5c0efed4f5867420b8c6ad1aff588718ed92c
Atchuthan, Sodexis <atchuthan@sodexis.com>
2021-02-05 16:21:07 +0530

    Added PR#661 and PR#662

--- mail_activity_board/__manifest__.py
+++ mail_activity_board/__manifest__.py
@@ -1,4 +1,5 @@
 # Copyright 2018 David Juaneda - <djuaneda@sdi.es>
+# Copyright 2021 Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Mail Activity Board",
@@ -7,10 +8,10 @@
     "development_status": "Beta",
     "category": "Social Network",
     "website": "https://github.com/OCA/social",
-    "author": "SDi, David Juaneda, Odoo Community Association (OCA)",
+    "author": "SDi, David Juaneda, Sodexis, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "depends": ["calendar", "board"],
     "data": ["views/templates.xml", "views/mail_activity_view.xml"],
-    "qweb": ["static/src/xml/inherit_chatter.xml"],
+    "qweb": ["static/src/components/chatter_topbar/chatter_topbar.xml"],
 }
